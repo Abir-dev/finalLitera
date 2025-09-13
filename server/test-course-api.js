753aa0099b1ddc0b8db2a2b4e5e3291dafaf2ae5
Archishman Mitra <riju.archishman@gmail.com>
2025-09-13 16:46:36 +0530

    Update API URL configuration in test-course-api.js to support environment variable for production deployment.

--- server/test-course-api.js
+++ server/test-course-api.js
@@ -1,7 +1,7 @@
 // Simple test script to verify course API endpoints
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 // Test data
 const testCourse = {
